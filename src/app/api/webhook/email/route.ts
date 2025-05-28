57b54912052486d3e6701789599dd18483c8da7f
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-28 12:30:24 +0530

    Removed unused type
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/api/webhook/email/route.ts
+++ src/app/api/webhook/email/route.ts
@@ -1,9 +1,5 @@
 import type { NextApiRequest } from "next";
 
-type ResponseData = {
-  message: string;
-};
-
 export async function POST(req: NextApiRequest) {
   return new Response(JSON.stringify({ message: "Sending email..." }), {
     status: 200,
